b246d65acaa3213ed67869d604d9f11e5bbac14b
Matt Wittmann <mwittmann@relaytx.com>
2024-01-23 09:11:51 -0800

    Add simple example executable

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import Data.Graph.Random
+import MCIS.McSplit
+import System.Random
+
 main :: IO ()
-main = undefined
+main =
+  let randomGraph = erdosRenyiGnp 8 0.2
+   in do
+        setStdGen (mkStdGen 4)
+        g1 <- randomGraph
+        g2 <- randomGraph
+        let mappings = MCIS.McSplit.mcis g1 g2
+        putStrLn $ "Largest mapping size: " ++ show (length (head mappings))
+        putStrLn $ "Number of largest mappings: " ++ show (length mappings)
