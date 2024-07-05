2e9ab52966745e973576bfc354abb76e9035b993
Matt Wittmann <mcwitt@gmail.com>
2024-07-05 12:07:50 -0700

    Clean

--- src/MCIS/McSplit.hs
+++ src/MCIS/McSplit.hs
@@ -1,22 +1,21 @@
 module MCIS.McSplit (mcis) where
 
-import Control.Monad (when)
-import Control.Monad.State
+import Control.Monad.State (State, evalState, state)
 import Data.Array ((!))
 import Data.Function (on)
 import Data.Graph (Graph, Vertex, vertices)
 import Data.List (delete, foldl', partition)
-import Data.Map.Strict (Map)
-import Data.Map.Strict qualified as Map
-import Data.Tree
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Tree (Tree (Node), foldTree)
 
 type Mapping = Map Vertex Vertex
 
 data McSplit = McSplit Mapping [([Vertex], [Vertex])]
 
 bound :: McSplit -> Int
-bound (McSplit mapping labelClasses) =
-  length mapping + sum [(min `on` length) vs1 vs2 | (vs1, vs2) <- labelClasses]
+bound (McSplit m cs) =
+  length m + sum [(min `on` length) vs1 vs2 | (vs1, vs2) <- cs]
 
 adj :: Graph -> Vertex -> Vertex -> Bool
 adj g v1 v2 = v2 `elem` (g ! v1)
@@ -25,29 +24,29 @@ extend :: Graph -> Graph -> McSplit -> State Int [McSplit]
 extend g1 g2 = go
   where
     go (McSplit _ []) = return []
-    go (McSplit mapping (([], _) : labelClasses)) = go (McSplit mapping labelClasses)
-    go (McSplit mapping ((_, []) : labelClasses)) = go (McSplit mapping labelClasses)
-    go (McSplit mapping ((v1 : v1s, v2s) : labelClasses)) = do
-      let mappingSize = length mapping
-      incumbentSize <- get
-      when (mappingSize > incumbentSize) (put mappingSize)
-      return
-        ( McSplit mapping ((v1s, v2s) : labelClasses)
-            : [ next
-                | v2 <- v2s,
-                  let next =
-                        McSplit
-                          (Map.insert v1 v2 mapping)
-                          ( concatMap
-                              ( \(u1s, u2s) ->
-                                  let (u1sa, u1sb) = partition (adj g1 v1) u1s
-                                      (u2sa, u2sb) = partition (adj g2 v2) u2s
-                                   in [(u1sa, u2sa), (u1sb, u2sb)]
-                              )
-                              ((v1s, delete v2 v2s) : labelClasses)
-                          ),
-                  bound next >= incumbentSize
-              ]
+    go (McSplit m (([], _) : cs)) = go (McSplit m cs)
+    go (McSplit m ((_, []) : cs)) = go (McSplit m cs)
+    go (McSplit m ((v1 : v1s, v2s) : cs)) =
+      state
+        ( \n ->
+            ( McSplit m ((v1s, v2s) : cs)
+                : [ next
+                    | v2 <- v2s,
+                      let next =
+                            McSplit
+                              (Map.insert v1 v2 m)
+                              ( concatMap
+                                  ( \(u1s, u2s) ->
+                                      let (u1sa, u1sb) = partition (adj g1 v1) u1s
+                                          (u2sa, u2sb) = partition (adj g2 v2) u2s
+                                       in [(u1sa, u2sa), (u1sb, u2sb)]
+                                  )
+                                  ((v1s, delete v2 v2s) : cs)
+                              ),
+                      bound next >= n
+                  ],
+              max (length m) n
+            )
         )
 
 generateM :: (Monad m) => (a -> m [a]) -> a -> m (Tree a)
@@ -57,8 +56,9 @@ generateM children node = do
   return (Node node subtrees)
 
 mcisSearchTree :: Graph -> Graph -> Tree McSplit
-mcisSearchTree g1 g2 = flip evalState 0 $ generateM (extend g1 g2) root
+mcisSearchTree g1 g2 = evalState search 0
   where
+    search = generateM (extend g1 g2) root
     root = McSplit Map.empty [(vertices g1, vertices g2)]
 
 largestBy :: (a -> a -> Ordering) -> [a] -> [a]
